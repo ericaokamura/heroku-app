9daf489c9581d675c77e22c1336885f099f0a6db
ericaokamura <erica.okamura@yahoo.com.br>
2023-04-08 00:30:49 -0300

    adicionando marker no mapa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     var mapa = document.createElement("img");
-    mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=tM2t0S2QKRlFMmVE7uqcu6BmgkuLY0nO&center=New+York&size=360,350`;
+    mapa.src = `https://www.mapquestapi.com/staticmap/v5/map?key=tM2t0S2QKRlFMmVE7uqcu6BmgkuLY0nO&locations=Los+Angeles,CA||San+Diego,CA||San+Jose,CA&defaultMarker=marker-num&size=360,350`;
     document.querySelector('#mapa').appendChild(mapa);
 }
