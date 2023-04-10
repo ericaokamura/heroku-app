ea9a5c0d4a24764eda9a44a4da83e02487462a84
Luiz LIma <lima.luizgo@gmail.com>
2023-04-10 19:23:52 -0300

    Update page.js

--- src/page.js
+++ src/page.js
@@ -17,32 +17,32 @@ const hotels = [
   new Hotel(
     'hotel-1',
     'Pullman Paris Tour Eiffel',
-    '18 Avenue De Suffren, 22 Rue Jean Rey Entrée Au, 75015, Paris, França',
-    '+33 1 44 38 56 00'
+    ' 18 Avenue De Suffren, 22 Rue Jean Rey Entrée Au, 75015, Paris, França',
+    ' +33 1 44 38 56 00'
   ),
   new Hotel(
     'hotel-2',
     'Hôtel Rosalie',
-    '8 Bis Av. de la Soeur Rosalie, 75013 Paris, França',
-    '+33 1 43 36 62 00'
+    ' 8 Bis Av. de la Soeur Rosalie, 75013 Paris, França',
+    ' +33 1 43 36 62 00'
   ),
   new Hotel(
     'hotel-3',
     "L'Empire Paris",
-    "48 Rue de l'Arbre Sec, 75001 Paris, França",
-    '+33 1 40 15 06 06'
+    " 48 Rue de l'Arbre Sec, 75001 Paris, França",
+    ' +33 1 40 15 06 06'
   ),
   new Hotel(
     'hotel-4',
     'Pavillon Monceau Hotel',
-    "43 Rue Jouffroy d'Abbans, 75017 Paris, França",
-    '+33 1 56 79 25 00'
+    " 43 Rue Jouffroy d'Abbans, 75017 Paris, França",
+    ' +33 1 56 79 25 00'
   ),
   new Hotel(
     'hotel-5',
     'Hôtel Château Frontenac',
-    '54 Rue Pierre Charron, 75008 Paris, França',
-    '+33 1 53 23 13 00'
+    ' 54 Rue Pierre Charron, 75008 Paris, França',
+    ' +33 1 53 23 13 00'
   ),
 ];
 
