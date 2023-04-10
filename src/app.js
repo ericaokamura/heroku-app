553e75a688f2ae35ba6eddd5ac364ffaa1cc9705
Luiz LIma <lima.luizgo@gmail.com>
2023-04-10 17:10:08 -0300

    Update

--- src/app.js
+++ src/app.js
@@ -8,24 +8,15 @@ const apiKey = 'AIzaSyC7K0CfjjrLQ6kocgQ7pGT0S0DvLIdAM2I';
 const geolocationApiUrl =
   'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
-function updateMap(location) {
-  if (mapIFrame) {
-    const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${location}`;
-    mapIFrame.setAttribute('src', mapUrl);
-  }
-}
-
 function getLocation() {
   if (!navigator.geolocation) {
     console.error('Geolocation is not supported by this browser.');
     return;
   }
-
   navigator.geolocation.getCurrentPosition(
     async (position) => {
       const { latitude, longitude } = position.coords;
       const apiUrl = `${geolocationApiUrl}?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
-
       try {
         const response = await fetch(apiUrl);
         const data = await response.json();
@@ -45,6 +36,22 @@ function getLocation() {
       console.error(error);
     }
   );
+}
+
+function saveLocationToStorage(location) {
+  localStorage.setItem('location', location);
+}
+
+function updateMap() {
+  if (mapIFrame) {
+    const cachedLocation = localStorage.getItem('location');
+    const location = cachedLocation || locationInput.value;
+    const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${location}`;
+    mapIFrame.setAttribute('src', mapUrl);
+    if (!cachedLocation) {
+      saveLocationToStorage(location);
+    }
+  }
 }
 
 function setDefaultDates() {
@@ -68,28 +75,20 @@ function showMessage() {
 }
 
 async function initialize() {
-  getLocation();
+  const cachedLocation = localStorage.getItem('location');
+  if (cachedLocation) {
+    locationInput.value = cachedLocation;
+  } else {
+    getLocation();
+  }
   setDefaultDates();
-  updateMap(locationInput.value);
-  searchButton.addEventListener('click', showMessage);
+  updateMap();
+  searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showMessage();
+  });
 }
 
 initialize().then(() => {
   console.log('Initialized!');
 });
-
-// Comentários explicativos abaixo
-//
-// LOCATION_INPUT_ID: id do elemento de entrada de localização
-// STARTING_DATE_INPUT_ID: id do elemento de entrada de data de partida
-// RETURNING_DATE_INPUT_ID: id do elemento de entrada de data de retorno
-// API_KEY: chave de API do Google Maps usada para incorporar o mapa na página
-// MAP_ID: id do elemento iframe que contém o mapa incorporado
-//
-// updateMap(): função que atualiza o mapa incorporado de acordo com o valor do campo de entrada de localização na página
-//
-// getLocation(): função que usa o recurso de geolocalização do navegador para obter a cidade, estado e país do usuário e preenche o campo de entrada de localização na página e atualiza o mapa
-//
-// setDefaultDates(): função que define as datas de partida e retorno padrão para a data atual e a data atual + 1 mês
-//
-// showMessage(): função que é chamada quando o botão de pesquisa é clicado, ela coleta as informações de tipo de viagem, localização e datas selecionadas e exibe uma mensagem de alerta com essas informações, além de atualizar o mapa.
